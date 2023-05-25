c2d4d53d842746f9e9c548d924717fb00dda6731
Vladislav <vlad_gogo@mail.ru>
2023-05-25 23:53:57 +0300

    Add tests

--- src/tests/test_tasks.py
+++ src/tests/test_tasks.py
@@ -9,4 +9,17 @@ async def test_get_tasks():
     async with AsyncClient(app=app, base_url="http://127.0.0.1:8000") as ac:
         response = await ac.post("/tasks/get_tasks/", json={'build': 'write_beautiful'})
     assert response.status_code == 200
-    # assert response.json() == {"message": "Tomato"}
+
+
+@pytest.mark.anyio
+async def test_get_tasks_with_error():
+    async with AsyncClient(app=app, base_url="http://127.0.0.1:8000") as ac:
+        response = await ac.post("/tasks/get_tasks/", json={'build': 'qwerty'})
+    assert response.status_code == 500
+
+
+@pytest.mark.anyio
+async def test_get_tasks_error_with_wrong_body():
+    async with AsyncClient(app=app, base_url="http://127.0.0.1:8000") as ac:
+        response = await ac.post("/tasks/get_tasks/", json={'name': 'qwerty'})
+    assert response.status_code == 422
